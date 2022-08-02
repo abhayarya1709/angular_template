550c86c9c279dd5d812c697a065c13aa7c0291aa
abhayarya1709 <abhayarya2002@gmail.com>
2022-08-02 22:34:03 +0530

    new repo

---
+++ src/app/app.routing.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+
+import { FullComponent } from './layouts/full/full.component';
+import { LandingComponent } from './landing/landing.component';
+
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: '',
+        loadChildren:
+          () => import('./material-component/material.module').then(m => m.MaterialComponentsModule)
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+      }
+    ]
+  },
+  { path: 'landing', component: LandingComponent },
+];
